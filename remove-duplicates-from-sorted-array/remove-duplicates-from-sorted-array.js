131a7f8b417e168f36ec6d9c020940e5b72bc77e
Ben Black <benblack7@gmail.com>
2021-11-07 16:35:25 -0600

    Update remove-duplicates-from-sorted-array.js

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -4,14 +4,10 @@
  */
 var removeDuplicates = function(nums) {
     for (i = 0; i < nums.length; i++) {
-        //console.log(i);
       if (nums[i+1] === nums[i]) {
         var spliced = nums.splice(i+1,1)
-        //console.log(spliced);
-        //nums.push(spliced);
         i--;  
-    }
-    }
-    //console.log(nums);
-    return nums.length;
+      }
+    }  
+  return nums.length;
 }
